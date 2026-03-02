2c14771e73be4cc515b15ab9cb9718cc0badcf4a
Gabbi-1373 <emillygabrielly03082009@gmail.com>
2026-03-02 16:58:50 -0300

    a

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-alert('o javascript funcionou!')
-
 function somar(){
     let num
     let num1
     num = document.getElementById('1').value
     num1 = document.getElementById('2').value
-    console.log(num) 
-    console.log(num10)
+    console.log(num + num1)
 }
