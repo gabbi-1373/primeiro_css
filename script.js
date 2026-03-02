32f5799bd7c33d09d92a990f99eb8d1caebffa85
Gabbi-1373 <emillygabrielly03082009@gmail.com>
2026-03-02 17:21:16 -0300

    subtrair e multiplicar

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ function somar(){
     let num1
     num = parseInt(document.getElementById('1').value)
     num1 = parseInt(document.getElementById('2').value)
+    console.log(num + num1)
+}
+function subtrair(){
+    let num
+    let num1
+    num = parseInt(document.getElementById('3').value)
+    num1 = parseInt(document.getElementById('4').value)
     console.log(num - num1)
+}
+
+function multiplicar(){
+    let num
+    let num1
+    num = parseInt(document.getElementById('5').value)
+    num1 = parseInt(document.getElementById('6').value)
     console.log(num * num1)
 }
