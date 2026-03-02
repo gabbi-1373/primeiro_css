0755f05e4aa8893408b8197b8c8d25cb0fff459e
Gabbi-1373 <emillygabrielly03082009@gmail.com>
2026-03-02 17:24:46 -0300

    document

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ function somar(){
     num1 = parseInt(document.getElementById('2').value)
     console.log(num + num1)
 }
+
 function subtrair(){
     let num
     let num1
     num = parseInt(document.getElementById('3').value)
     num1 = parseInt(document.getElementById('4').value)
+    document 
     console.log(num - num1)
 }
 
@@ -18,5 +20,8 @@ function multiplicar(){
     let num1
     num = parseInt(document.getElementById('5').value)
     num1 = parseInt(document.getElementById('6').value)
+
     console.log(num * num1)
 }
+
+
