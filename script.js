42262c643ab57ee16aaceb80b1f3b05b4f8f77b4
Gabbi-1373 <emillygabrielly03082009@gmail.com>
2026-03-02 16:54:46 -0300

    criando o html, css e o js e escrevendo os códigos dentro deles.

---
+++ script.js
@@ -0,0 +1,10 @@
+alert('o javascript funcionou!')
+
+function somar(){
+    let num
+    let num1
+    num = document.getElementById('1').value
+    num1 = document.getElementById('2').value
+    console.log(num) 
+    console.log(num10)
+}
