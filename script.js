6702e6385cdf3225d245c5daf06502204d430419
Gabbi-1373 <emillygabrielly03082009@gmail.com>
2026-03-02 17:03:02 -0300

    mudança de sniais

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ function somar(){
     let num1
     num = parseInt(document.getElementById('1').value)
     num1 = parseInt(document.getElementById('2').value)
-    console.log(num + num1)
+    console.log(num - num1)
+    console.log(num * num1)
 }
